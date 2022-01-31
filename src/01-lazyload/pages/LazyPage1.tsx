8b273b4ab55f9179242e4c071f60effdf386bf4a
MatiasBaDev <matiasbadev@gmail.com>
2022-01-31 12:28:46 -0300

    first commit

---
+++ src/01-lazyload/pages/LazyPage1.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export const LazyPage1 = () => {
+  return <div>LazyPage1</div>;
+};
+
---
+++ src/01-lazyload/pages/LazyPage2.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export const LazyPage2 = () => {
+  return <div>LazyPage2</div>;
+};
+
